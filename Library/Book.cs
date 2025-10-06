a2ee837a3f14978741d46b8f6c9a0f1fe10c1975
B00988811 <singh-m21@ulster.ac.uk>
2025-10-06 15:19:19 +0100

    initial

---
+++ Library/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Library
+{
+    
+}
+internal class Book
+{
+    public string Title;
+    public string Author;
+    public string ISBN;
+
+    static void Main(string[] args)
+    {
+        Book myBook = new Book();
+
+        myBook.Title = "C# for beginners";
+        myBook.Author = "BillGates";
+        myBook.ISBN = "12345678";
+
+        Console.WriteLine("Title: " + myBook.Title);
+        Console.WriteLine("Author: " + myBook.Author);
+        Console.WriteLine("ISBN: " + myBook.ISBN);
+    }
+}
