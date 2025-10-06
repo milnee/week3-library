74781602c7287855cec4576e09bab66599e2c60d
B00988811 <singh-m21@ulster.ac.uk>
2025-10-06 15:37:22 +0100

    added a constructor to the book class

--- Library/Book.cs
+++ Library/Book.cs
@@ -8,40 +8,29 @@ namespace Library
         public string Author;
         public string ISBN;
 
+        // Constructor that requires title, author, and ISBN
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
         void DisplayInfo()
         {
             Console.WriteLine($"Book title: {Title}");
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
-
         }
 
         static void Main(string[] args)
         {
+            Book book1 = new Book("C# for beginners", "BillGates", "12345678");
+            Book book2 = new Book("C#", "Microsoft", "1234");
 
-            Book book1 = new Book();
-            book1.Title = "C# for beginners";
-            book1.Author = "BillGates";
-            book1.ISBN = "12345678";
-
-            // book 2
-            Book book2 = new Book();
-            book2.Title = "C#";
-            book2.Author = "Microsoft";
-            book2.ISBN = "1234";
-
-            // simpler way to displayinfo instead of writing Console.WriteLine each time
             book1.DisplayInfo();
             book2.DisplayInfo();
-
-
         }
-
-
-
-
-
-
-   }
+    }
 }
